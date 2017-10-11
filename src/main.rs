ff68aa334c221c3a160dd0a4e99273a7f8dc1b66
LeopoldArkham <leopold.arkham@gmail.com>
2017-10-11 18:02:01 +0200

    main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::net::TcpListener;
 use std::error::Error;
 use std::io::{Read, Write};
+use std::env::args;
+
+const BUF: usize = 512;
 
 fn main() {
     if let Err(e) = run() {
@@ -8,17 +11,22 @@ fn main() {
     }
 }
 
-fn run () -> Result<(), Box<Error>> {
-    let listner = TcpListener::bind("127.0.0.1:1256")?;
+fn run() -> Result<(), Box<Error>> {
+    let port = args().nth(1).unwrap_or("8080".into());
+    let listner = TcpListener::bind(format!("127.0.0.1:{}", port))?;
+    
     for incoming in listner.incoming() {
         let mut incoming = incoming?;
-        println!("Connection from: {}", incoming.peer_addr()?);
-        let mut buf = [0; 512];
+        println!("Connection from: {}:", incoming.peer_addr()?);
+        let mut buf = [0; BUF];
         let read = incoming.read(&mut buf)?;
-        print!("Request: {}", String::from_utf8_lossy(&buf));
-        incoming.write(&buf[..read])?;
-        incoming.flush()?;
-        println!("Closing Connection\n");
+        if read == BUF {
+            println!("Warning: Request body truncated.");
+        }
+        println!("  Request: {:?}", String::from_utf8_lossy(&buf[..read]));
+        incoming.write_all(&buf[..read])?;
+        println!("  Closing Connection\n");
     }
+
     Ok(())
 }
